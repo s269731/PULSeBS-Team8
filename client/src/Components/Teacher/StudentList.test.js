217bc8d73bdc1d3632c45a9575a4f2382a88afaa
S274294 <62012194+S274294@users.noreply.github.com>
2020-11-18 21:49:38 +0800

    Update StudentList.test.js

--- client/src/Components/Teacher/StudentList.test.js
+++ client/src/Components/Teacher/StudentList.test.js
@@ -1,6 +1,5 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { } from "history";
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import StudentList from "./StudentList";
