97ab4990f24a1a487fc8a2936fe0195199d37758
Daniele <s280077@studenti.polito.it>
2020-11-22 15:45:24 +0100

    PUL-77 Code review (story 8)

--- server/src/services/email.js
+++ server/src/services/email.js
@@ -10,87 +10,53 @@ const mail = nodemailer.createTransport({
   },
 });
 
-/*function start() {
-  const now = new Date();
-  const end_of_today = new Date(now);
-  end_of_today.setHours(23, 00, 00, 000);
-  setTimeout(sendingEmailBookedPeople, end_of_today.getTime() - now.getTime());
-}*/
+function sendEmail(to, subject, text) {
+  const mailOptions = {
+    from: config.mailer.email,
+    to,
+    subject,
+    text,
+  };
+  mail.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(`Email sent: ${info.response}`);
+    }
+  });
+}
 
 function sendingEmailBookedPeople() {
   lecturesDao.getTeachersForEmail().then((array) => {
     // array.push({email_addr:"test@mail.com", booked_people:123, subject:"TEST"});
     array.forEach((elem) => {
-      const mailOptions = {
-        from: config.mailer.email,
-        to: elem.email_addr,
-        subject: 'Booked people for tomorrow lesson',
-        text: `There are ${elem.booked_people} people booked for the lesson: ${elem.subject} of tomorrow`,
-      };
-
-      mail.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(`Email sent: ${info.response}`);
-        }
-      });
+      sendEmail(elem.email_addr, 'Booked people for tomorrow lesson',
+        `There are ${elem.booked_people} people booked for the lesson: ${elem.subject} of tomorrow`);
     });
   });
-  //setTimeout(sendingEmailBookedPeople, 8.64e+7); // set timeout for the following day
 }
 
 async function sendBookingConfirmationEmail(lectureId, userId) {
   const information = await lecturesDao.getInfoBookingConfirmation(lectureId, userId);
   if (information.email) {
-    const mailOptions = {
-      from: config.mailer.email,
-      to: information.email,
-      subject: 'Booking confirmation',
-      text: `You have been successfully booked for the ${information.subject}'s lesson. Date: ${information.date_hour}, Class: ${information.class}`,
-    };
-
-    mail.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log(`Email sent: ${info.response}`);
-      }
-    });
+    sendEmail(information.email, 'Booking confirmation',
+      `You have been successfully booked for the ${information.subject}'s lesson.\nDate: ${information.date_hour}, Class: ${information.class}`);
   }
 }
 
 async function sendingEmailCancelledLecture(students) {
   if (students.length > 0) {
     const info = students.shift();
-    const subject = info.subject;
-    const teacher = info.teacher;
-    const date_hour = info.date_hour;
-
-    let fields = date_hour.split("T");
-    let date = fields[0];
-    let hour = fields[1].split(".")[0].split(":")[0] + ":" + fields[1].split(".")[0].split(":")[1];
+    const { subject, teacher, date_hour } = info;
+    const dateString = (new Date(date_hour)).toLocaleString('en');
 
     students.forEach((elem) => {
-      const mailOptions = {
-        from: config.mailer.email,
-        to: elem.email_addr,
-        subject: 'Lecture has been cancelled',
-        text: `The lecture "${subject}" teached by professor ${teacher} and scheduled for ${date} at ${hour} has been cancelled`,
-      };
-
-      mail.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(`Email sent: ${info.response}`);
-        }
-      });
+      sendEmail(elem.email_addr, 'Lecture has been cancelled',
+        `The lecture "${subject}" teached by professor ${teacher} and scheduled for ${dateString} has been cancelled`);
     });
   }
 }
 
-//exports.start = start;
 exports.sendBookingConfirmationEmail = sendBookingConfirmationEmail;
 exports.sendingEmailBookedPeople = sendingEmailBookedPeople;
 exports.sendingEmailCancelledLecture = sendingEmailCancelledLecture;
