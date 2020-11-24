c7a81c311ba0fb7094f658c995d150072fb1481d
S274294 <62012194+S274294@users.noreply.github.com>
2020-11-24 12:55:42 +0800

    Delete db.js

--- .github/server/src/db.js
+++
@@ -1,6 +0,0 @@
-const config = require('config');
-const Sqlite = require('better-sqlite3');
-
-const DB = new Sqlite(config.DBSOURCE, config.verbose ? { verbose: console.log } : null);
-
-module.exports = DB;
