8f73748810e3bd7beea7248afc62bc5b3d4c1bca
Daniele <s280077@studenti.polito.it>
2020-12-01 19:34:08 +0100

    revert commit 94aaea5ec73256fdff7d124830e35f036a5d0955

--- client/src/Components/Teacher/Graphs/LecturesGraph.js
+++ client/src/Components/Teacher/Graphs/LecturesGraph.js
@@ -102,7 +102,7 @@ class LecturesGraph extends Component {
 
              	]
 		}
-         {
+		const options2 = {
         			theme: "light1", // "light1", "dark1", "dark2"
         			animationEnabled: true,
         			zoomEnabled: true,
