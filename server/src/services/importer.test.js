77eced273b09e9c06744c5eaa30209c1b99be3ae
s269731 <s269731@studenti.polito.it>
2020-12-08 18:30:05 +0100

    fix: importer.test.js

--- server/src/services/importer.test.js
+++ server/src/services/importer.test.js
@@ -6,7 +6,7 @@ afterAll(() => {
 });
 
 test('Should import students successfully', async () => {
-  fs.writeFileSync('importer.test.csv', '900026', 'Nicla', 'Baresi', 'Montemarciano', 's900026@students.politu.it', '1991-11-14', 'BY75859842');
+  fs.writeFileSync('importer.test.csv', '900026, Nicla, Baresi, Montemarciano, s900026@students.politu.it, 1991-11-14, BY75859842');
   const res = await importer.importFile({ tempFilePath: 'importer.test.csv' }, 'students');
   expect(res).toBe(true);
 });
@@ -22,7 +22,7 @@ test('Should fail to import students when providing malformed csv file', async (
 });
 
 test('Should import teachers successfully', async () => {
-  fs.writeFileSync('importer.test.csv', 'd9047', 'Gherardo', 'Capon', 'Gherardo.Capon@politu.it', 'RG17768024');
+  fs.writeFileSync('importer.test.csv', 'd9047, Gherardo, Capon, Gherardo.Capon@politu.it, RG17768024');
   const res = await importer.importFile({ tempFilePath: 'importer.test.csv' }, 'students');
   expect(res).toBe(true);
 });
@@ -38,7 +38,7 @@ test('Should fail to import teachers when providing malformed csv file', async (
 });
 
 test('Should import courses successfully', async () => {
-  fs.writeFileSync('importer.test.csv', 'XY7032', 3, 2, 'Sistemi energetici industriali', 'd9034');
+  fs.writeFileSync('importer.test.csv', 'XY7032, 3, 2, Sistemi energetici industriali, d9034');
   const res = await importer.importFile({ tempFilePath: 'importer.test.csv' }, 'courses');
   expect(res).toBe(true);
 });
@@ -54,15 +54,15 @@ test('Should fail to import courses when providing malformed csv file', async ()
 });
 
 test('Should import enrollments successfully', async () => {
-  fs.writeFileSync('importer.test.csv', 'XY1211', '902772');
-  const res = await importer.importFile({ tempFilePath: 'importer.test.csv' }, 'lectures');
+  fs.writeFileSync('importer.test.csv', 'XY1211, 902772');
+  const res = await importer.importFile({ tempFilePath: 'importer.test.csv' }, 'enrollments');
   expect(res).toBe(true);
 });
 
 test('Should fail to import enrollments when providing malformed csv file', async () => {
   fs.writeFileSync('importer.test.csv', '112,1,30,2020-12-01T13:00:00.000Z,In person');
   try {
-    const res = await importer.importFile({ tempFilePath: 'importer.test.csv' }, 'lectures');
+    const res = await importer.importFile({ tempFilePath: 'importer.test.csv' }, 'enrollments');
     expect(res).toBeFalsy();
   } catch (err) {
     expect(err).toBeTruthy();
@@ -70,15 +70,15 @@ test('Should fail to import enrollments when providing malformed csv file', asyn
 });
 
 test('Should import schedules successfully', async () => {
-  fs.writeFileSync('importer.test.csv', 'XY3731', 5, 'Wed', 70, '10:00-11:30');
-  const res = await importer.importFile({ tempFilePath: 'importer.test.csv' }, 'classes');
+  fs.writeFileSync('importer.test.csv', 'XY3731, 5, Wed, 70, 10:00-11:30');
+  const res = await importer.importFile({ tempFilePath: 'importer.test.csv' }, 'schedules');
   expect(res).toBe(true);
 });
 
 test('Should fail to import schedules when providing malformed csv file', async () => {
   fs.writeFileSync('importer.test.csv', '11,12A');
   try {
-    const res = await importer.importFile({ tempFilePath: 'importer.test.csv' }, 'classes');
+    const res = await importer.importFile({ tempFilePath: 'importer.test.csv' }, 'schedules');
     expect(res).toBeFalsy();
   } catch (err) {
     expect(err).toBeTruthy();
