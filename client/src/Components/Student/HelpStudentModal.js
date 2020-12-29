6f0755bba8dc455ad988a17cb7f13a701b330a3c
S274294 <s274294@studenti.polito.it>
2020-12-29 23:10:58 +0800

    add tutorial content in student/teacher page

--- client/src/Components/Student/HelpStudentModal.js
+++ client/src/Components/Student/HelpStudentModal.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import {Button, Modal} from "react-bootstrap";
 import Jumbotron from "../../assets/helpF.png";
+import filterPng from "../../assets/stu/01.png";
+import bookPng from "../../assets/stu/02.png";
+import cancelPng from "../../assets/stu/03.png";
+import calendarPng from "../../assets/stu/04.png";
 
 function HelpStudentModal(props) {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => {setShow(true)};
-  
+    const titStyle = {textAlign:'left',margin:'20px 0 10px',fontSize:24}
     return (
       <>
         <Button data-testid="help-button" variant="link">
@@ -29,7 +33,16 @@ function HelpStudentModal(props) {
             <Modal.Title>Tutorial</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            There is nothing difficult, for understanding you just need to check the page
+            <div style={{textAlign:'center'}}>
+              <h3 style={titStyle}>1.To filter courses,you can click "course name" you wanted</h3>
+              <img style={{maxWidth:'100%'}} src={filterPng} alt=""/>
+              <h3 style={titStyle}>2.To book a lecture,you can click on "Book now "button</h3>
+              <img style={{maxWidth:'100%'}} src={bookPng} alt=""/>
+              <h3 style={titStyle}>3.To cancel a lecture,you can click on "Cancel reservation" button</h3>
+              <img style={{maxWidth:'100%'}} src={cancelPng} alt=""/>
+              <h3 style={titStyle}>4.To get your Calendar,you can click on "Calendar"</h3>
+              <img style={{maxWidth:'100%'}} src={calendarPng} alt=""/>
+            </div>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
--- client/src/Components/Teacher/HelpTeacherModal.js
+++ client/src/Components/Teacher/HelpTeacherModal.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import {Button, Modal} from "react-bootstrap";
 import Jumbotron from "../../assets/helpF.png";
+import filterPng from "../../assets/tea/01.png";
+import opePng from "../../assets/tea/02.png";
+import pastPng from "../../assets/tea/03.png";
+import statisticsPng from "../../assets/tea/041.png";
+import bookingPng from "../../assets/tea/042.png";
+import attendancePng from "../../assets/tea/043.png";
 
 function HelpTeacherModal(props) {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => {setShow(true)};
-  
+    const titStyle = {textAlign:'left',margin:'20px 0 10px',fontSize:24}
     return (
       <>
         <Button data-testid="help-button" variant="link">
@@ -29,7 +35,21 @@ function HelpTeacherModal(props) {
             <Modal.Title>Tutorial</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            There is nothing difficult, for understanding you just need to check the page
+            <div style={{textAlign:'center'}}>
+              <h3 style={titStyle}>1.To filter courses,you can click "course name" you wanted</h3>
+              <img style={{maxWidth:'100%'}} src={filterPng} alt=""/>
+              <h3 style={titStyle}>2.To get booked students list,you can click on"Booked students" button;  To cancel a lecture,you can click on "Cancel lecture" button; To modify the lecture modality,you can click on "Switch to online lecture" button</h3>
+              {/* <h3 style={titStyle}>2.2To cancel a lecture,you can click on"Cancel lecture"button</h3>
+              <h3 style={titStyle}>2.3To modify the lecture modality,you can click on"Switch to online lecture"button</h3> */}
+              <img style={{maxWidth:'100%'}} src={opePng} alt=""/>
+              <h3 style={titStyle}>3.To see past lectures,you can click on"My past lectures"button</h3>
+              <img style={{maxWidth:'100%'}} src={pastPng} alt=""/>
+              <h3 style={titStyle}>4.1 To see the Statistics of course,you can click on "Statistics" button</h3>
+              <img style={{maxWidth:'100%'}} src={statisticsPng} alt=""/>
+              <h3 style={titStyle}>4.2 Statistics includes Booking Statistics and Attendance Statistics,you can click "Booking Statistics " and "Attendance Statistics" buttons to get detailed data</h3>
+              <img style={{maxWidth:'100%'}} src={bookingPng} alt=""/>
+              <img style={{maxWidth:'100%'}} src={attendancePng} alt=""/>
+            </div>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
