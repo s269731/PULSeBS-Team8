f171a8b0c54f7a603202c1a3d8a653d7567dbc2c
S274294 <s274294@studenti.polito.it>
2021-01-09 21:32:27 +0800

    update toturial content

--- client/src/Components/Student/HelpStudentModal.js
+++ client/src/Components/Student/HelpStudentModal.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {Button, Modal} from "react-bootstrap";
+import {Button, Modal, Carousel} from "react-bootstrap";
 import Jumbotron from "../../assets/helpF.png";
 import filterPng from "../../assets/stu/01.png";
 import bookPng from "../../assets/stu/02.png";
@@ -11,6 +11,8 @@ function HelpStudentModal(props) {
     const handleClose = () => setShow(false);
     const handleShow = () => {setShow(true)};
     const titStyle = {textAlign:'left',margin:'20px 0 10px',fontSize:24}
+    const btnStyle = {position:'absolute',bottom:0}
+
     return (
       <>
         <Button data-testid="help-button" variant="link">
@@ -33,7 +35,31 @@ function HelpStudentModal(props) {
             <Modal.Title>Tutorial</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <div style={{textAlign:'center'}}>
+          <Carousel 
+            interval={30000} 
+            style={{height:430}} 
+            prevIcon={<Button style={btnStyle}>Prev</Button>} 
+            nextIcon={<Button style={btnStyle}>Next</Button>}
+          >
+            <Carousel.Item style={ { textAlign: 'center' } }>
+            <h3 style={titStyle}>1.To filter courses,you can click "course name" you wanted</h3>
+              <img style={ { maxWidth: '100%' } } src={ filterPng } alt="" />
+            </Carousel.Item>
+            <Carousel.Item style={ { textAlign: 'center' } }>
+              <h3 style={titStyle}>2.To book a lecture,you can click on "Book now "button</h3>
+              <img style={ { maxWidth: '100%' } } src={ bookPng } alt="" />
+            </Carousel.Item>
+            <Carousel.Item style={ { textAlign: 'center' } }>
+            <h3 style={titStyle}>3.To cancel a lecture,you can click on "Cancel reservation" button</h3>
+              <img style={ { maxWidth: '100%' } } src={ cancelPng } alt="" />
+            </Carousel.Item>
+            <Carousel.Item style={ { textAlign: 'center' } }>
+            <h3 style={titStyle}>4.To get your Calendar,you can click on "Calendar"</h3>
+              <img style={ { maxWidth: '100%' } } src={ calendarPng } alt="" />
+            </Carousel.Item>
+           
+          </Carousel>
+            {/* <div style={{textAlign:'center'}}>
               <h3 style={titStyle}>1.To filter courses,you can click "course name" you wanted</h3>
               <img style={{maxWidth:'100%'}} src={filterPng} alt=""/>
               <h3 style={titStyle}>2.To book a lecture,you can click on "Book now "button</h3>
@@ -42,7 +68,7 @@ function HelpStudentModal(props) {
               <img style={{maxWidth:'100%'}} src={cancelPng} alt=""/>
               <h3 style={titStyle}>4.To get your Calendar,you can click on "Calendar"</h3>
               <img style={{maxWidth:'100%'}} src={calendarPng} alt=""/>
-            </div>
+            </div> */}
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
